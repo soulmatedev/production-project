85f642fcc057f364d77269f1f43b12fc79bdb485
soulmatedev <khokhlov.dev@gmail.com>
2024-03-01 21:39:07 +0500

    Организует стили и добавляет темную и светлую тему

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React, {Suspense} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
-import './index.scss';
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import './styles/index.scss';
+import {useTheme} from "./theme/useTheme";
 
 const App = () => {
+	const { theme, toggleTheme } = useTheme();
+
 	return (
-		<div>
+		<div className={`app ${theme}`}>
+			<button onClick={toggleTheme}>Toggle</button>
 			<Link to={'/'}>Главная</Link>
 			<Link to={'/about'}>О сайте</Link>
 			<Suspense fallback={<div>Loading...</div>}>
