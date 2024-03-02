44992ce2607ee40cd122741eea62519746b65f58
soulmatedev <khokhlov.dev@gmail.com>
2024-03-02 18:42:59 +0500

    Добавляет хелпер для работы с классами

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import './styles/index.scss';
 import {useTheme} from "./theme/useTheme";
+import {classNames} from "./helpers/classNames/classNames";
 
 const App = () => {
 	const { theme, toggleTheme } = useTheme();
 
 	return (
-		<div className={`app ${theme}`}>
+		<div className={classNames('app', {}, [theme])}>
 			<button onClick={toggleTheme}>Toggle</button>
 			<Link to={'/'}>Главная</Link>
 			<Link to={'/about'}>О сайте</Link>
