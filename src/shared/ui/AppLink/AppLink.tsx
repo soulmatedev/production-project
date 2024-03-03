db96d243dd8b971001213030fa6ee07bae2e6de2
soulmatedev <khokhlov.dev@gmail.com>
2024-03-03 20:51:07 +0500

    Добавляет виджет навигации

---
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,34 @@
+import {classNames} from "shared/lib/classNames/classNames";
+import styles from './AppLink.module.scss';
+import {Link, LinkProps} from "react-router-dom";
+import {FC} from "react";
+
+export enum AppLinksTheme {
+	PRIMARY = 'primary',
+	SECONDARY = 'secondary',
+}
+
+interface AppLinksProps extends LinkProps {
+	className?: string,
+	theme?: AppLinksTheme
+}
+
+export const AppLink: FC<AppLinksProps> = (props) => {
+	const {
+		to,
+		className,
+		children,
+		theme = AppLinksTheme.PRIMARY,
+		...otherProps
+	} = props;
+
+	return (
+		<Link
+			to={to}
+			className={classNames(styles.AppLink, {}, [className, styles[theme]])}
+			{...otherProps}
+		>
+			{children}
+		</Link>
+	);
+};
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {classNames} from "shared/lib/classNames/classNames";
+import styles from './Navbar.module.scss';
+import {AppLink, AppLinksTheme} from "shared/ui/AppLink/AppLink";
+
+interface NavbarProps {
+	className?: string,
+}
+
+export const Navbar = ({className}: NavbarProps) => {
+	return (
+		<div className={classNames(styles.Navbar, {}, [className])}>
+			<div className={styles.links}>
+				<AppLink theme={AppLinksTheme.SECONDARY} to={'/'} className={styles.mainLink}>
+					Главная
+				</AppLink>
+				<AppLink theme={AppLinksTheme.SECONDARY} to={'/about'}>
+					О сайте
+				</AppLink>
+			</div>
+		</div>
+	);
+};
+
