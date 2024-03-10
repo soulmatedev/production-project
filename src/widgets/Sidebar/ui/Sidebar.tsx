81e6e7c20037b489ec6f99ac4be6ce06123d95e6
soulmatedev <khokhlov.dev@gmail.com>
2024-03-10 21:04:58 +0500

    Добавляет интернационализацию

--- src/widgets/Sidebar/ui/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar.tsx
@@ -1,14 +1,15 @@
-import {classNames} from "shared/lib/classNames/classNames";
+import { classNames } from 'shared/lib/classNames/classNames';
 import styles from './Sidebar.module.scss';
 import React, {useState} from "react";
 import {ThemeSwitcher} from "widgets/ThemeSwitcher";
+import LangSwitcher from "widgets/LangSwitcher/LangSwitcher";
 
 interface SidebarProps {
-	className?: string,
+	className?: string;
 }
 
 export const Sidebar = ({className}: SidebarProps) => {
-	const [collapsed, setCollapsed] = useState(false);
+	const [collapsed, setCollapsed] = useState(false)
 
 	const onToggle = () => {
 		setCollapsed(prev => !prev);
@@ -21,7 +22,9 @@ export const Sidebar = ({className}: SidebarProps) => {
 			<button onClick={onToggle}>toggle</button>
 			<div className={styles.switchers}>
 				<ThemeSwitcher />
+				<LangSwitcher className={styles.lang}/>
 			</div>
 		</div>
 	);
 };
+
