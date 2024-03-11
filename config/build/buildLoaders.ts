c63fcdea72a608cc012175fb091033111d277f94
soulmatedev <khokhlov.dev@gmail.com>
2024-03-11 21:34:30 +0500

    PP-17 Добавляет babel и extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -8,6 +8,25 @@ export function buildLoaders(isDev: BuildOptions): webpack.RuleSetRule[] {
 		use: ['@svgr/webpack'],
 	}
 
+	const babelLoader = {
+		test: /\.(js|jsx|tsx)$/,
+		exclude: /node_modules/,
+		use: {
+			loader: "babel-loader",
+			options: {
+				presets: ['@babel/preset-env'],
+				"plugins": [
+					["i18next-extract",
+						{
+							locales: ['ru', 'en'],
+							keyAsDefaultValue: true,
+						}
+					],
+				]
+			}
+		}
+	}
+
 	const cssLoaders = {
 		test: /\.s[ac]ss$/i,
 		use: [
@@ -45,6 +64,7 @@ export function buildLoaders(isDev: BuildOptions): webpack.RuleSetRule[] {
 	return [
 		svgLoader,
 		fileLoader,
+		babelLoader,
 		typescriptLoader,
 		cssLoaders
 	];
