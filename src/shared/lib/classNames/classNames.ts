2263f95bec049a8c593155060623f9778f32cbb0
soulmatedev <khokhlov.dev@gmail.com>
2024-03-14 18:52:53 +0500

    Добавляет jest

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -6,9 +6,7 @@ export function classNames(cls: string, mods: Mods = {}, additional: string[] = 
         ...additional.filter(Boolean),
         ...Object.entries(mods)
             .filter(([_, value]) => Boolean(value))
-            .map(([_]) => classNames),
+            .map(([className]) => className),
     ]
         .join(' ');
 }
-
-classNames('remove-btn', { hovered: true, selectable: true, red: true }, ['pdg']);
