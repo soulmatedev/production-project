a598056124b340a520d259111a2b46ff7cc05c2d
soulmatedev <khokhlov.dev@gmail.com>
2024-03-15 19:52:27 +0500

    Добавляет страницу для несуществующих маршрутов

---
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import styles from './NotFoundPage.module.scss';
+
+interface NotFoundPageProps {
+	className?: string
+}
+
+const NotFoundPage = ({ className }: NotFoundPageProps) => {
+    const { t } = useTranslation();
+
+    return (
+        <div className={classNames(styles.NotFoundPage, {}, [className])}>
+            {t('Страница не найдена')}
+        </div>
+    );
+};
+
+export default NotFoundPage;
--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,15 +1,20 @@
 import { RouteProps } from 'react-router-dom';
 import { MainPage } from 'pages/MainPage';
 import { AboutPage } from 'pages/AboutPage';
+import { NotFoundPage } from 'pages/NotFoundPage';
 
 export enum AppRoutes {
 	MAIN = 'main',
 	ABOUT = 'about',
+    NOT_FOUND = 'not_found'
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
     [AppRoutes.MAIN]: '/',
     [AppRoutes.ABOUT]: '/about',
+
+    // Будет вызываться, если ни одни другой маршрут не отработал.
+    [AppRoutes.NOT_FOUND]: '*',
 };
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
@@ -17,8 +22,14 @@ export const routeConfig: Record<AppRoutes, RouteProps> = {
         path: RoutePath.main,
         element: <MainPage />,
     },
+
     [AppRoutes.ABOUT]: {
         path: RoutePath.about,
         element: <AboutPage />,
+    },
+
+    [AppRoutes.NOT_FOUND]: {
+        path: RoutePath.not_found,
+        element: <NotFoundPage />,
     },
 };
