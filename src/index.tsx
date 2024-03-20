b5125c6e74063407bc72b1f75ed7de9184e011cd
soulmatedev <khokhlov.dev@gmail.com>
2024-03-20 19:14:54 +0500

    Добавляет ErrorBoundary для отлавливания ошибок

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,17 @@ import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
 import React from 'react';
 import App from 'app/App';
+import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 
 import 'shared/config/i18n/i18n';
 
 render(
     <BrowserRouter>
-        <ThemeProvider>
-            <App />
-        </ThemeProvider>
+        <ErrorBoundary>
+            <ThemeProvider>
+                <App />
+            </ThemeProvider>
+        </ErrorBoundary>
     </BrowserRouter>,
     document.getElementById('root'),
 );
