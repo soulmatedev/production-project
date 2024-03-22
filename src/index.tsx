11ea41aada0a468f01a5c178a1ce741f02d0c6c5
soulmatedev <khokhlov.dev@gmail.com>
2024-03-22 16:05:19 +0500

    Добавляет плагин для анализа bundle

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,9 @@ import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
 import React from 'react';
 import App from 'app/App';
-import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 
 import 'shared/config/i18n/i18n';
+import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 
 render(
     <BrowserRouter>
